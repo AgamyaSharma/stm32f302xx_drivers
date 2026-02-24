736049f40ffcc7f440550dd943a936ae9f624f07
Agamya Sharma <agamya.005@gmail.com>
2026-02-24 21:04:48 +0530

    ADDED SPI MACROS

--- drivers/Inc/stm32f302xx_spi_driver.h
+++ drivers/Inc/stm32f302xx_spi_driver.h
@@ -16,7 +16,35 @@ typedef struct{
 	SPI_RegDef_t *pSPIx;
 	SPI_Config_t SPI_Config;
 }SPI_Handle_t;
-#endif /* INC_STM32F302XX_SPI_DRIVER_H_ */
+
+#define SPI_DEVICE_MODE_MASTER					1
+#define SPI_DEVICE_MODE_MASTER					0
+
+#define SPI_BUS_CONFIG_FD						1
+#define SPI_BUS_CONFIG_HD						2
+#define SPI_BUS_CONFIG_SIMPLEX_TX				3
+#define SPI_BUS_CONFIG_SIMPLEX_RX				4
+
+#define SPI_SCLK_SPEED_DIV2						0
+#define SPI_SCLK_SPEED_DIV4						1
+#define SPI_SCLK_SPEED_DIV8						2
+#define SPI_SCLK_SPEED_DIV16					3
+#define SPI_SCLK_SPEED_DIV32					4
+#define SPI_SCLK_SPEED_DIV64					5
+#define SPI_SCLK_SPEED_DIV128					6
+#define SPI_SCLK_SPEED_DIV256					7
+
+#define SPI_DFF_8BITS							0
+#define SPI_DFF_16BITS							1
+
+
+#define SPI_CPOL_HIGH							1
+#define SPI_CPOL_LOW							0
+
+#define SPI_CPHA_HIGH							1
+#define SPI_CPHA_LOW							0
+
+
 
 
 void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
@@ -35,3 +63,7 @@ void SPI_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi);
 
 
 void SPI_IRQHandle(SPI_Handle *pSPIHandle );
+
+#endif /* INC_STM32F302XX_SPI_DRIVER_H_ */
+
+
